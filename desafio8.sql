c34b7389db9a0eb2398bfd81886c722452874b00
Victor Emmanuel <victorpassos2012@gmail.com>
2022-03-11 01:35:57 -0300

    Adcionado o ORDER BY no desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,3 +1,4 @@
 SELECT ar.Artist_Name AS 'artista', al.Album_Name AS 'album' FROM SpotifyClone.Artist AS ar
 JOIN SpotifyClone.Album AS al ON ar.Artist_ID = al.Artist_ID
-WHERE ar.Artist_Name = 'Walter Phoenix';
+WHERE ar.Artist_Name = 'Walter Phoenix'
+ORDER BY al.Album_Name ASC;
