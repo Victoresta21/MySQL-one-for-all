ac60eec5d582fed79047859a721e0eb7a687bc9a
Victor Emmanuel <victorpassos2012@gmail.com>
2022-03-11 01:34:35 -0300

    Desafio 8 feito

---
+++ desafio8.sql
@@ -0,0 +1,3 @@
+SELECT ar.Artist_Name AS 'artista', al.Album_Name AS 'album' FROM SpotifyClone.Artist AS ar
+JOIN SpotifyClone.Album AS al ON ar.Artist_ID = al.Artist_ID
+WHERE ar.Artist_Name = 'Walter Phoenix';
